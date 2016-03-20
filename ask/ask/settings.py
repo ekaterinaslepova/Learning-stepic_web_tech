0fe0e3f946bf149f7344ce6d9f76c44f3dcc1e14
Ekaterina Slepova <Ekaterina Slepova>
2016-03-20 15:37:56 +0500

    db settings

--- ask/ask/settings.py
+++ ask/ask/settings.py
@@ -75,9 +75,17 @@ WSGI_APPLICATION = 'ask.wsgi.application'
 # https://docs.djangoproject.com/en/1.9/ref/settings/#databases
 
 DATABASES = {
+    # 'default': {
+    #     'ENGINE': 'django.db.backends.sqlite3',
+    #     'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+    # }
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'db_qa',
+        'USER': 'root',
+        # 'PASSWORD': 'password',
+        'HOST': 'localhost',
+        'PORT': '',
     }
 }
 
--- init.sh
+++ init.sh
@@ -4,3 +4,5 @@ sudo /etc/init.d/nginx restart
 #sudo ln -s /home/box/web/etc/hello.py   /etc/gunicorn.d/hello.py
 #sudo /etc/init.d/gunicorn /home/box/web/hello:application --bind 0.0.0.0:8080
 sudo /etc/init.d/gunicorn ask.wsgi:application --bind 0.0.0.0:8000
+
+mysql -uroot -e "create database db_qa"
